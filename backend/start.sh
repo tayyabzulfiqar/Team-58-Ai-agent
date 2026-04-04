0b7a060a10e7f9fe2dbf14307246543d508e7752
tayyabzulfiqar <152661932+tayyabzulfiqar@users.noreply.github.com>
2026-04-04 20:19:38 +0500

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,3 +1,3 @@
-pip install -r requirements.txt
 #!/bin/bash
-exec uvicorn scripts.api.server:app --host 0.0.0.0 --port $PORT --workers 2
+pip install -r requirements.txt
+exec uvicorn main:app --host 0.0.0.0 --port $PORT
